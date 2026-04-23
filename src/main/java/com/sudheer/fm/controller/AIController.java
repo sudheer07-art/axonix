85a01866ee1023483086c775ebb62d5aba00007d
sudheer gits <sudheercheerla07@icloud.com>
2026-04-23 08:18:13 +0530

    fixed gemini model

--- src/main/java/com/sudheer/fm/controller/AIController.java
+++ src/main/java/com/sudheer/fm/controller/AIController.java
@@ -129,7 +129,7 @@ public class AIController {
 
 
                     String url =
-                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
+                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key="
                             + apiKey;
 
 
